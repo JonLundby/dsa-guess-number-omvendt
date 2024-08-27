c4076025d113211591a34dade87933d8519ba508
Jon <lundbyjon@hotmail.com>
2024-08-27 20:58:08 +0200

    fixed starting game again when guess is correct

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 
 window.addEventListener("load", startApp);
 
+let guess;
+
 function startApp() {
-    console.log("app kører!")
     hideGameElements();
     document.querySelector("#start-btn").addEventListener("click", startGame);
+    document.querySelector("#guesses").classList.add("hidden");
 }
 
 function hideGameElements() {
-    document.querySelector("#reply-btns").classList.add("hidden")
-    document.querySelector("#guesses").classList.add("hidden")
+    document.querySelector("#reply-btns").classList.add("hidden");
 }
 
 function showGameElements() {
@@ -20,5 +21,31 @@ function showGameElements() {
 
 function startGame() {
     document.querySelector("#start-paragraph").classList.add("hidden");
+    document.querySelector("#btn-too-high").addEventListener("click", guessNumber)
+    document.querySelector("#btn-too-low").addEventListener("click", guessNumber)
+    document.querySelector("#btn-correct").addEventListener("click", correctGuess)
+
+    document.querySelector("#guesses").innerHTML = "";
+
     showGameElements();
+    guessNumber();
+}
+
+function guessNumber() {
+    guess = Math.ceil(Math.random() * 100);
+    const html = `
+                <li>Jeg gætter på ${guess}</li>
+                `;
+    
+    document.querySelector("#guesses").insertAdjacentHTML("afterbegin", html);
+}
+
+function correctGuess() {
+    const html = `
+                <li>Hah, gotcha!! It was ${guess} :)</li>
+                `;
+
+    document.querySelector("#guesses").insertAdjacentHTML("afterbegin", html);
+    hideGameElements();
+    document.querySelector("#start-paragraph").classList.remove("hidden");
 }
